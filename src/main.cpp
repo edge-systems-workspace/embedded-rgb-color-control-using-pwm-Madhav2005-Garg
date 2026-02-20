dd7040ddb4c9f11ea240d7d5e1ab1921e0dc4622
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-20 22:19:10 +0530

    implement PWM brightness control and color transitions

--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,44 @@
 #include <Arduino.h>
+/**
+ * @file main.cpp
+ * @brief Embedded RGB LED Control (Digital + PWM)
+ */
 
 #define RED_PIN 9
 #define GREEN_PIN 10
 #define BLUE_PIN 11
 
-// ... definitions from previous commit ...
-
 void setup() {
     Serial.begin(9600);
-
     pinMode(RED_PIN, OUTPUT);
     pinMode(GREEN_PIN, OUTPUT);
     pinMode(BLUE_PIN, OUTPUT);
-
     Serial.println("RGB LED Control System - Initialized");
-    delay(1000);
 }
 
-// ... setup code from above ...
-
 void loop() {
-    // Basic Digital Toggle for testing hardware
+    // --- DIGITAL MODE ---
     digitalWrite(RED_PIN, HIGH);
     delay(1000);
     digitalWrite(RED_PIN, LOW);
+    delay(1000);
+
+    // --- ANALOG (PWM) MODE ---
+    // Red Full
+    analogWrite(RED_PIN, 255);
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 0);
+    delay(1000);
+
+    // Green Full
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 255);
+    analogWrite(BLUE_PIN, 0);
+    delay(1000);
+
+    // Blue Full
+    analogWrite(RED_PIN, 0);
+    analogWrite(GREEN_PIN, 0);
+    analogWrite(BLUE_PIN, 255);
     delay(1000);
 }
